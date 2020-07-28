71c9910898a5b3db78af93b5d967ee1b7d57e0ec
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-07-28 22:24:36 +0530

    Added useRef to prevent request on every letter typed

--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,23 +1,28 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 import Card from '../UI/Card';
 import './Search.css';
 
 const Search = React.memo((props) => {
+  const searchRef = useRef();
   const [filterState, setFilterState] = useState('');
   // Props destructuring
   // Fetchin g props data and storing it in a variable
   // This allows useEffect to run only when taht specific thing changes
   const { onLoadIngredients } = props;
 
+  // Now the function will check if the inout has changed in a 0.5 sec interval and then run nstead of sending request in every keystroke
   useEffect(() => {
-    console.log('Executed');
+    setTimeout(() => {
+      if(filterState === searchRef.current.value) {
+        console.log('Executed');
     const query =
       filterState.length === 0
         ? ''
         : `?orderBy="title"&equalTo="${filterState}"`;
     fetch(
-      'https://emerald-mission-191715.firebaseio.com/ingredients.json' + query)
+      'https://emerald-mission-191715.firebaseio.com/ingredients.json' + query
+    )
       .then((response) => response.json())
       .then((responseData) => {
         const loadedIngredients = [];
@@ -30,13 +35,16 @@ const Search = React.memo((props) => {
         }
         onLoadIngredients(loadedIngredients);
       });
-  }, [filterState, onLoadIngredients]);
+      }
+    }, 500);
+  }, [filterState, onLoadIngredients, searchRef]);
   return (
     <section className='search'>
       <Card>
         <div className='search-input'>
           <label>Filter by Title</label>
           <input
+            ref={searchRef}
             type='text'
             value={filterState}
             onChange={(event) => setFilterState(event.target.value)}
