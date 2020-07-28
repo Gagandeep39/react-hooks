d635d02066008629ad4bca2bba89998b95f60bd0
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-07-28 15:23:18 +0530

    Added Ingredient list Added logic to add ingredient handler

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import IngredientForm from './IngredientForm';
 import Search from './Search';
+import IngredientList from './IngredientList';
 
 function Ingredients() {
+  const [ingredientsState, setIngredientsState] = useState([]);
+
+  const addIngredientHandler = (newIngredients) => {
+    setIngredientsState((previouseIngredients) => [
+      // Taking all previous ingreidents
+      ...previouseIngredients,
+      // Attaching id to new ingredient and adding it to the state
+      { id: Math.random().toString(), ...newIngredients },
+    ]);
+  };
+
   return (
-    <div className="App">
-      <IngredientForm />
+    <div className='App'>
+      <IngredientForm onAddIngredient={addIngredientHandler} />
 
       <section>
         <Search />
         {/* Need to add list here! */}
+        <IngredientList ingredients={ingredientsState} />
       </section>
     </div>
   );
